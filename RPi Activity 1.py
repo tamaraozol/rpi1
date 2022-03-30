4bb2ac9b46cae6f7b2c32abbcb56c452653712e0
tamaraozol <101278710+tamaraozol@users.noreply.github.com>
2022-03-30 09:59:17 -0500

    Add files via upload

---
+++ RPi Activity 1.py
@@ -0,0 +1,6 @@
+####################################################
+# Name: Tamara Ozol and Nathan Mitchell
+# Date: 3/30/2022
+# Description: RPi Activity 1
+####################################################
+from tkinter import *
